b0c1e4e59db7cb2af8162e58954b688793fe21b7
luciano <luc.castellucci@gmail.com>
2025-09-30 18:57:41 -0300

    commit array

--- Introduction/src/ArrayCart.java
+++ Introduction/src/ArrayCart.java
@@ -55,6 +55,7 @@ public class ArrayCart {
 	driver.manage().window().maximize();
 	//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+	System.out.println("test");
 	
 	driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 	
